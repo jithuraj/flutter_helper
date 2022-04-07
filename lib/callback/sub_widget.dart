ad36de8b1eaa3faabd6363b478d3cf96f8c20d27
jithuraj <jithuigate@gmail.com>
2022-04-07 12:26:34 +0530

    need to call widget. for statefull widget

--- lib/callback/sub_widget.dart
+++ lib/callback/sub_widget.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
 
-class SubWidget extends StatelessWidget {
+class SubWidget extends StatefulWidget {
   Function(String) retunData;
 
   SubWidget(this.retunData);
 
   @override
+  State<SubWidget> createState() => _SubWidgetState();
+}
+
+class _SubWidgetState extends State<SubWidget> {
+  @override
   Widget build(BuildContext context) {
     return Container(
       child: TextField(
         onChanged: (value) {
-          retunData(value);
+          widget.retunData(value);
         },
       ),
     );
